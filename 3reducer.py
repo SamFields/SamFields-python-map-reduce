836dd3eaf6ec9849d51b671a66702e10f7adc8b3
Sam Fields <s526635@nwmissouri.edu>
2020-02-12 13:15:11 -0600

    Put all h03 files in

---
+++ 3reducer.py
@@ -0,0 +1,27 @@
+s = open("02.txt","r")
+r = open("03.txt", "w")
+
+thisKey = ""
+thisValue = 0
+
+for line in s:
+  data = line.strip().split('\t')
+  paymentType, cost = data
+
+  if paymentType != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = paymentType
+    thisValue = 0
+  
+  # apply the aggregation function
+  thisValue += 1
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+s.close()
+r.close()
---
+++ README.md
@@ -0,0 +1,3 @@
+# 44-517 h03 Working with Python
+
+Sammy Fields Module 2 challenge repo (h03)
